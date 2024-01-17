5ecd9d4ae8d6557b9e1b364098bfbeaf16460622
lovinaj <lovejon218@gmail.com>
2024-01-17 03:12:40 -0800

    updated

--- models/state.py
+++ models/state.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
+""" State Module for HBNB project """
+
+from sqlalchemy import Column
+# from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
+import os
+
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
+    """ State class """
+
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    cities = relationship('City', cascade='delete', backref='state')
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':  # FileStorage
+        @property
+        def cities(self):
+            """getter attribute for cities
 
+            Return:
+                return the list of City instances
+            """
+
+            c_list = []
+            a_list = models.storage.all(City)
+
+            for city in a_list.values():
+                if city.state_id == self.id:
+                    c_list += city
+
+            return c_list
